e641a4bad46472036d948fd5479e8a9c77217789
Mdhamza77 <mmohammedhamza333@gmail.com>
2023-02-10 15:17:57 +0530

    All Changes Completed , last commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import 'semantic-ui-css/semantic.min.css'
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom' ;
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-      <App />
+    <App />
   </BrowserRouter>
-
-
 );
 
 // If you want to start measuring performance in your app, pass a function
